2367fc7864d71ce9ef8bcd5e9f6dd55965462f8a
Piotr Pikuła <piotrpikula@gmail.com>
2018-09-04 18:47:01 +0200

    change application folder to app/
    Developers good practice.

--- src/index.php
+++ src/index.php
@@ -3,3 +3,4 @@ $now = new DateTime();
 
 echo '<p>Inside php. Now is '.$now->format('Y-m-d H:i:s'). ' , timezone is '.$now->getTimezone()->getName().'</p>';
 
+echo '<p>Application folder: '.__DIR__.'</p>';
